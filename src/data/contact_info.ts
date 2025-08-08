77ab475742bdeb4c2734437642a6ea17ce7969de
Novice Engineer <skyler32702@gmail.com>
2025-08-08 15:57:07 -0500

    Added Logo; Better Props Declaration; Abstraction of Embed Components; Carousel Component File (Missing Implementation

---
+++ src/data/contact_info.ts
@@ -0,0 +1,11 @@
+export interface ContactInfo {
+    phone: string;
+    email: string;
+};
+
+const chn_contact_info: ContactInfo = {
+    phone: "(580) 235-7999",
+    email: "athenadr71@gmail.com",
+};
+
+export default chn_contact_info;
